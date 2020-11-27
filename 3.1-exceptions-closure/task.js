df1fee219b5eaecea1dad0b0f363d1eafdb21af2
Ekaterina Babiy <eka_babiy@mail.ru>
2020-11-27 16:34:54 +0300

    Выполнено

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,60 @@
+"use strict";
+
+//  Задание 1
+
+function parseCount(value) {
+    if (isNaN(Number.parseInt(value))) {
+        throw new Error("Невалидное значение")
+    }
+    return Number.parseInt(value);
+};
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+   		 } catch (error) {
+        return error;
+    }
+};
+
+//  Задание 2
+
+class Triangle {
+	constructor(a, b, c) {
+	this.a = a;
+	this.b = b;
+    this.c = c;
+    	if ((a + b) < c || (b + c) < a || (c + a) < b) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+	}
+
+getPerimeter() {
+    return Number(this.a + this.b + this.c);
+}
+
+getArea() {
+	const p = this.getPerimeter() / 2;
+    return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+}
+
+}
+
+function getTriangle(a, b, c) {
+	try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            getArea() {
+                return "Ошибка! Треугольник не существует";
+            },
+            getPerimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
+
+//проверка
+console.log(getTriangle(3, 4, 5));
+console.log(getTriangle(2, 2, 5));
