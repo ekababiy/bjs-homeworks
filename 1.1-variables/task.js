f1c0589610a3ba40bc47a6ef08e98c536991e6b6
Ekaterina Babiy <eka_babiy@mail.ru>
2020-11-30 16:54:46 +0300

    Выполнено

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,4 +1,5 @@
 function averageMark(a, g, p) {
+
  let algebra = a;
  let geography = g;
  let physics = p;
@@ -7,12 +8,14 @@ function averageMark(a, g, p) {
 };
 
 function sayHello(userName) {
+
 	let myName = userName;
 	let message = `Привет, мир! Меня зовут  ${userName}`;
 	return message;
 };
 
 function calculateFormula() {
+	
 let x = 2;
 let y = 22;
 let z = 0;
