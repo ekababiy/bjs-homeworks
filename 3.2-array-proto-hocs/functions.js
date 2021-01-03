df7f7a36a0348d3464830ecaef3994304f9ddd00
Ekaterina Babiy <eka_babiy@mail.ru>
2021-01-03 18:23:29 +0300

    3,2 дз

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -14,27 +14,31 @@ new Bow()
 ];
  
 function getNames() {
-	return weapons.map((weapon) => weapon.name);
+	return weapons.map(weapon => weapon.name);
 }
+//console.log(getNames());
+
 
 function getCountReliableWeapons(durability) {
-	return weapons.filter((weapon) => weapon.durability > durability).length;
+	return weapons.filter(weapon => weapon.durability > durability).length;
 }
+//console.log(getCountReliableWeapons(250));
 
 function hasReliableWeapons(durability) {
-	return weapons.some((weapon) => weapon.durability > durability);
+	return weapons.some(weapon => weapon.durability > durability);
 }
+//console.log(hasReliableWeapons(800));
 
 function getReliableWeaponsNames(durability) {
-	return weapons
-	.filter((weapon) => weapon.durability > durability)
- 	.map((weapon) => weapon.name);
+	return weapons.filter(weapon => weapon.durability > durability).map((weapon) => weapon.name);
 }
+//console.log(getReliableWeaponsNames(300));
 
 function getTotalDamage() {
- 	return weapons.reduce(function (acc, curr) {
- 	return acc + curr.attack;}, 0);
+ return weapons.reduce((totalDamage, weapon) => totalDamage += weapon.attack, 0)
 }
+//console.log(getTotalDamage());
+
 
 function getValuestCountToSumValues (arr, neededSum) {
  	const values = [];
@@ -59,15 +63,15 @@ getValuestCountToSumValues([1,2,3,5,2,7,3,5,2], 10);
 //возвращает 6, т.к. 1+2+3+5+2=13 (5 чисел), а 1+2+3+5+2+7=20 (6 чисел)
 getValuestCountToSumValues([1,2,3,5,2,7,3,5,2], 20);
 
-/*
-//Задача №2
 
+//Задача №2
+//2.1.1
 function sleep(milliseconds) 
 {
   let e = new Date().getTime() + milliseconds;
   while (new Date().getTime() <= e) {}
 }
-
+//2.1.2
 function sum(...args) {
   // Замедление на половину секунды.
   sleep(100); // Можно использовать другое значение замедления.
@@ -75,10 +79,11 @@ function sum(...args) {
     return sum += +arg;
   }, 0);
 }
-
- function compareArrays( arr1, arr2 ) {}
-
-
+//2.1.3
+ function compareArrays( arr1, arr2 ) {
+ 	return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+ }
+ 
  //проверка
 compareArrays([8, 9], [6]); // false, разные значения
 compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
@@ -88,7 +93,22 @@ compareArrays([8, 1, 2], [8, 1, 2]); // true
 
 
 
-function memorize(fn, limit) {}
+//2.2 и 2.2,2
+function memorize(fn, limit) {
+const memory = [];
+return function (...args) {	
+let arr = args;
+let prop = memory.find(obj => compareArrays(obj.args, arr));
+if (prop) {
+return prop.result;	
+}
+let result = fn(...args);
+memory.push({ args, result })
+if (memory.length > limit) {
+memory.shift();
+}
+return result;
+}
 
 
 //проверка
@@ -101,8 +121,3 @@ sum(3, 4); // 7
   и повторно не делает вычисления
  */
 //mSum(3, 4); // 7
-
-
-
-
-//Задача №3
